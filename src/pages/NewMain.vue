192084cab2437d6921ec8f948845725a7ab91d3b
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-07-09 08:40:15 +0300

    added midi support
    Co-Authored-By: Bluskript <52386117+Bluskript@users.noreply.github.com>

--- src/pages/NewMain.vue
+++ src/pages/NewMain.vue
@@ -1,12 +1,42 @@
 <script lang="ts" setup>
 import { onKeyDown, onKeyUp } from '@vueuse/core'
+import { onMounted } from 'vue'
 import Oscillators from '~/components/Main/Oscillators/Oscillators.vue'
 import Visuals from '~/components/Main/FlexZone/Visuals.vue'
 import Keyboard from '~/components/Main/Keyboard.vue'
-import { OscManager } from '~/logic/oscManager'
+import { oscManager } from '~/logic/oscManager'
 import { keys } from '~/logic/keysound'
 
-const oscManager = new OscManager()
+function onMIDIInit(midi) {
+  const midiAccess = midi
+
+  let haveAtLeastOneDevice = false
+  const inputs = midiAccess.inputs.values()
+  for (let input = inputs.next(); input && !input.done; input = inputs.next()) {
+    input.value.onmidimessage = MIDIMessageEventHandler
+    haveAtLeastOneDevice = true
+  }
+  if (!haveAtLeastOneDevice)
+    alert('No MIDI input devices present.  You\'re gonna have a bad time.')
+}
+
+function MIDIMessageEventHandler(event) {
+  // Mask off the lower nibble (MIDI channel, which we don't care about)
+  switch (event.data[0] & 0xF0) {
+    case 0x90:
+      if (event.data[2] != 0) { // if velocity != 0, this is a note-on message
+        oscManager.semitoneDown(event.data[1] - 36)
+      }
+      break
+      // if velocity == 0, fall thru: it's a note-off.  MIDI's weird, y'all.
+    case 0x80:
+      oscManager.semitoneUp(event.data[1] - 36)
+  }
+}
+
+onMounted(async() => {
+  onMIDIInit(await navigator.requestMIDIAccess())
+})
 
 onKeyDown(ev => keys[ev.key] !== undefined && !ev.repeat, (ev) => {
   oscManager.semitoneDown(keys[ev.key])
